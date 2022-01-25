f0e7edcb9a96e21c90e30e96bcb37cf74c8818bc
shmoopydisco <deadphaker@gmail.com>
2022-01-25 19:35:48 +0200

    add basic mutex to protect double process run

--- Project/Keyboard Locker/Program.cs
+++ Project/Keyboard Locker/Program.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace Keyboard_Locker
 {
     static class Program
     {
-        [STAThread]
+        
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool createdNew = true;
+            using (Mutex mutex = new Mutex(true, "MyApplicationName", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+                else
+                {
+                    Process current = Process.GetCurrentProcess();
+                    foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                    {
+                        if (process.Id != current.Id)
+                        {
+                            SetForegroundWindow(process.MainWindowHandle);
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
